2285507dc833bb054b7c722856537ea9f18fd444
Dududess <pierrebeaubecq@hotmail.fr>
2025-06-16 11:18:19 +0200

    Fix chargement modèle

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from app.api import router as api_router
 import os
+import json
 import firebase_admin
 from firebase_admin import credentials
+from app.ml_utils import load_model_and_mapping
+
 print("🚀 Launching FastAPI app with PORT =", os.environ.get("PORT"))
 app = FastAPI(
     title="Carder Recommendation API",
@@ -11,10 +14,22 @@ app = FastAPI(
     debug=True
 )
 try:
-    cred = credentials.Certificate("/secrets/GOOGLE_APPLICATION_CREDENTIALS")
+    firebase_key = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+    firebase_key_dict = json.loads(firebase_key)
+    cred = credentials.Certificate(firebase_key_dict)
     firebase_admin.initialize_app(cred)
-    print("✅ Firebase initialized")
+    print("Firebase initialized")
 except Exception as e:
-    print("❌ Firebase init failed:", e)
+    print("Firebase init failed:", e)
+    
+# ✅ Hook de démarrage
+@app.on_event("startup")
+async def startup_event():
+    try:
+        print("Chargement du modèle au démarrage...")
+        load_model_and_mapping([])
+        print("Modèle chargé")
+    except Exception as e:
+        print(f"Échec du chargement du modèle au démarrage : {e}")
     
 app.include_router(api_router)
